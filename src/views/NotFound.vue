80f7979e22f2020dd9dd4931c9afca95d2f92872
NhatTran2608 <112406282+NhatTran2608@users.noreply.github.com>
2023-04-14 12:00:32 +0700

    Tao trang xu ly loi

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="page">
+        <p>
+            <img src="https://cdn.tgdd.vn/hoi-dap/580732/loi-404-not-found-la-gi-9-cach-khac-phuc-loi-404-not-3-800x534.jpg"
+                alt="" style="width: 100%;">
+            <b style="text-align: center; margin: auto;"> Oops, không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ.</router-link></b> 
+        </p>
+    </div>
+</template>
+
+
